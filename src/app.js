448626dabaeec7364b419954f7285dda287e59a7
tamaramunoz <tami.skt@gmail.com>
2020-06-19 21:31:39 -0400

    adding enviroment variables

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 require('./database');
 
 // settings
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
     defaultLayout: 'main',
--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/gallery_photos_db', {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
