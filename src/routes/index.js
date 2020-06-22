66e766a3b9acbf47ec13f1973a48b543966f11d5
tamaramunoz <tami.skt@gmail.com>
2020-06-22 19:16:25 -0400

    showing swiper and function to delete in index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,7 @@ cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
-})
+});
 
 router.get('/', async(req, res) => {
     const photos = await Photo.find();
@@ -40,7 +40,6 @@ router.get('/images/add', async(req, res) => {
             }
         }
     })
-
 });
 
 router.post('/images/add', async(req, res) => {
@@ -54,7 +53,15 @@ router.post('/images/add', async(req, res) => {
     });
     await newPhoto.save();
     await fs.unlink(req.file.path);
-    res.send('Received');
+    res.redirect('/');
+});
+
+router.get('/images/delete/:photo_id', async(req, res) => {
+    const { photo_id } = req.params;
+    const photo = await Photo.findByIdAndDelete(photo_id);
+    const result = await cloudinary.v2.uploader.destroy(photo.public_id);
+    console.log(result);
+    res.redirect('/images/add');
 });
 
 module.exports = router;
